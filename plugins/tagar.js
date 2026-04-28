ed643d7fba29e92f698f0a261530f7336a63ca33
baggashermd1-crypto <baggashermd1@gmail.com>
2026-04-28 11:47:58 +0500

    Update tagar.js

--- plugins/tagar.js
+++ plugins/tagar.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 cmd({
     pattern: "ytv",
-    alias: ["ytmp5", "video"],
+    alias: ["ytmp4", "video"],
     desc: "Download YouTube video (MP4)",
     category: "download",
     react: "📹",
